ef4d574ea06160117932cf47799be12a259ef1f8
Dmitry Kovanikov <kovanikov@gmail.com>
2019-05-01 16:48:39 +0800

    [#6] Implement better error reporting
    Resolves #6

--- test/Spec.hs
+++ test/Spec.hs
@@ -11,7 +11,10 @@ import Hakyll.Shortcut.Links (applyAllShortcuts)
 
 main :: IO ()
 main =
-    runIOorExplode (readMarkdown def testMd) >>= runIOorExplode . writeMarkdown def . applyAllShortcuts >>= print
+    runIOorExplode (readMarkdown def testMd)
+    >>= \pandoc -> case applyAllShortcuts pandoc of
+        Left errs       -> mapM_ putStrLn errs
+        Right newPandoc -> runIOorExplode (writeMarkdown def newPandoc) >>= print
 
 testMd :: Text
 testMd = "This is [Hello](@github:kowainik) yes no [aelve/shortcut-links](@github)"
