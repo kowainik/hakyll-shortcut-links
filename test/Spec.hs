0c4e051f2a1e171a19f3e9dd86b001351b9fabee
Veronika Romashkina <vrom911@gmail.com>
2019-05-02 16:04:41 +0800

    Add unit test on parsing (#8)

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,16 +1,19 @@
 module Main (main) where
 
 import Data.Text (Text)
+import Test.Hspec (hspec)
 import Text.Pandoc.Class (runIOorExplode)
 import Text.Pandoc.Options (def)
 import Text.Pandoc.Readers.Markdown (readMarkdown)
 import Text.Pandoc.Writers.Markdown (writeMarkdown)
 
 import Hakyll.Shortcut.Links (applyAllShortcuts)
+import Test.Parser (parserSpecs)
 
 
 main :: IO ()
-main =
+main = do
+    hspec parserSpecs
     runIOorExplode (readMarkdown def testMd)
     >>= \pandoc -> case applyAllShortcuts pandoc of
         Left errs       -> mapM_ putStrLn errs
---
+++ test/Test/Parser.hs
@@ -0,0 +1,32 @@
+module Test.Parser
+       ( parserSpecs
+       ) where
+
+import Data.Either (isLeft)
+import Test.Hspec (Spec, describe, it, shouldBe, shouldSatisfy)
+
+import Hakyll.Shortcut.Links.Parser (parseShortcut)
+
+parserSpecs :: Spec
+parserSpecs = describe "links parsing" $ do
+    it "can parse name" $
+        parseShortcut "@github" `shouldBe`
+            Right ("github", Nothing, Nothing)
+    it "can parse name and option" $
+        parseShortcut "@github(kowainik)" `shouldBe`
+            Right ("github", Just "kowainik", Nothing)
+    it "can parse name,option and text" $
+        parseShortcut "@github(kowainik):hakyll-shortcut-links" `shouldBe`
+            Right ("github", Just "kowainik", Just "hakyll-shortcut-links")
+    it "can parse name and text" $
+        parseShortcut "@github:hakyll-shortcut-links" `shouldBe`
+            Right ("github", Nothing, Just "hakyll-shortcut-links")
+    it "doesn't parse link without @" $
+        parseShortcut "github" `shouldSatisfy` isLeft
+    it "doesn't parse not closed bracket in option" $
+        parseShortcut "@github(kowainik" `shouldSatisfy` isLeft
+    it "parse until valid symbol" $ do
+        parseShortcut "@github kowainik" `shouldBe` Right ("github", Nothing, Nothing)
+        parseShortcut "@github)" `shouldBe` Right ("github", Nothing, Nothing)
+    it "doesn't parse empty text" $
+        parseShortcut "@github:" `shouldSatisfy` isLeft
